49ff7986d585d40bd4bc6075e2d2603bec556854
Iryna Artymuk <artimyk@ukr.net>
2022-11-26 19:13:04 +0300

    Update task_5.js

--- JS/module_05/tasks/task_5.js
+++ JS/module_05/tasks/task_5.js
@@ -5,3 +5,15 @@
 // Функція повертає Імена юзерів (формат стрінги) в яких є ця книга ("Harry Potter") \\\ "Anna, Oleksii"
 
 // 2 Порахувати вік всіх юзерів у яких є ключ age.
+
+const friends = [
+    { name: 'Anna', books: ['Bible', 'Harry Potter'], age: 21 },
+    { name: 'Bob', books: ['War and peace', 'Romeo and Juliet'], age: 26 },
+    { name: 'Alice', books: ['War and peace', 'Romeo and Juliet'] },
+    {
+      name: 'Oleksii',
+      books: ['Bible', 'War and peace', 'Harry Potter', 'Romeo and Juliet'],
+      age: 26,
+    },
+];
+
